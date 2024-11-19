d964d6163647a173637319c5b27712c40094f13e
Bástyai Botond <64700017+botator@users.noreply.github.com>
2024-11-19 22:56:00 +0100

    Add files via upload

--- js.js
+++ js.js
@@ -1,19 +1,54 @@
 const colors = ["red","yellow","blue","green","brown","purple","orange"];
+let score = 0;
+let colorNumber ;
+let colorNumber2;
+let colorNumber3;
 
+function scoring(){
+    document.getElementById('score').innerHTML = "Score:   "+score
+}
 
 function yesno(){
-    let colorNumber = Math.floor(Math.random() * 7);
-    let colorNumber2 = Math.floor(Math.random() * 7);
+    colorNumber  = Math.floor(Math.random() * 7);
+    colorNumber2 = Math.floor(Math.random() * 7);
+    colorNumber3 = Math.floor(Math.random() * 7);
     document.getElementById('colorname').innerHTML = colors[colorNumber];
+    document.getElementById('colorcolor').innerHTML = colors[colorNumber3];
     document.getElementById('colorcolor').style = 'color :'+colors[colorNumber2];
-}
-function yes(){
 
 }
-function no(){
+function yes(){
     
+    if(colorNumber == colorNumber2)
+    {
+        score +=100;
+    }
+    else if(colorNumber!=colorNumber2 ){
+        score-=100;
+        if(score<0){
+            score+= (score * -1)
+        }
+    }
+    scoring();
+    yesno();
 }
-function start(){
+function no(){
+    if(colorNumber!=colorNumber2){
+        score +=100;
+    }
+    else if(colorNumber == colorNumber2 ){
+        score-=100;
+        if(score<0){
+            score+= (score * -1)
+        }
+    }
+    scoring();
+    yesno();
+}
+function startt(){
+    document.getElementById('main').style = 'visibility: visible;display:block;';
+    document.getElementById('start').style ='visibility: hidden;display:none;';
+    yesno();
     let countDownTime = 60;
     const countDownElement = document.getElementById('time');
     const interval = setInterval(function() {
@@ -25,3 +60,4 @@ function start(){
         }
     }, 1000);
 }
+
