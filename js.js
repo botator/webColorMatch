88bdef50f53347610b5e61495dbf2626474ade79
Bástyai Botond <64700017+botator@users.noreply.github.com>
2024-11-20 09:03:23 +0100

    Add files via upload

--- js.js
+++ js.js
@@ -1,9 +1,9 @@
 const colors = ["red","yellow","blue","green","brown","purple","orange"];
 let score = 0;
-let colorNumber ;
-let colorNumber2;
+let colorNumber =0;
+let colorNumber2 =1;
 let colorNumber3;
-
+let hp = 4;
 function scoring(){
     document.getElementById('score').innerHTML = "Score:   "+score
 }
@@ -15,16 +15,28 @@ function yesno(){
     document.getElementById('colorname').innerHTML = colors[colorNumber];
     document.getElementById('colorcolor').innerHTML = colors[colorNumber3];
     document.getElementById('colorcolor').style = 'color :'+colors[colorNumber2];
-
+    Gameover();
+}
+document.onkeydown =function(e){
+    var keyp =e.key;
+    if(keyp == 'ArrowLeft'){
+        no();
+    }
+    if(keyp == 'ArrowRight'){
+        yes();
+    }
 }
 function yes(){
     
     if(colorNumber == colorNumber2)
     {
         score +=100;
+        document.getElementById('isGood').innerHTML = '✓';
     }
     else if(colorNumber!=colorNumber2 ){
         score-=100;
+        document.getElementById('isGood').innerHTML = '✕';
+        hp--;
         if(score<0){
             score+= (score * -1)
         }
@@ -35,9 +47,12 @@ function yes(){
 function no(){
     if(colorNumber!=colorNumber2){
         score +=100;
+        document.getElementById('isGood').innerHTML = '✓';
     }
     else if(colorNumber == colorNumber2 ){
         score-=100;
+        document.getElementById('isGood').innerHTML = '✕';
+        hp--;
         if(score<0){
             score+= (score * -1)
         }
@@ -46,7 +61,9 @@ function no(){
     yesno();
 }
 function startt(){
-    document.getElementById('main').style = 'visibility: visible;display:block;';
+    hp = 4;
+    score = 0;
+    document.getElementById('main').style = "visibility: visible;display:block;";
     document.getElementById('start').style ='visibility: hidden;display:none;';
     yesno();
     let countDownTime = 60;
@@ -56,8 +73,20 @@ function startt(){
         countDownElement.textContent = 'Time: '+countDownTime  ;
         if (countDownTime <= 0) {
             clearInterval(interval);
-            countDownElement.textContent = 'Time is up!';
+            document.getElementById('main').style = 'visibility: hidden;display:none;';
+            document.getElementById('start').style ="visibility: visible;display:block;";
+            document.getElementById('final').innerHTML ="Final score: "+score;
+        }
+        else if(hp == 0){
+            clearInterval(interval);
+            document.getElementById('gameover').innerHTML = 'Game Over!!!!'
+            document.getElementById('main').style = 'visibility: hidden;display:none;';
+            document.getElementById('start').style ="visibility: visible;display:block;";
+            document.getElementById('final').innerHTML ="Final score: "+score; 
         }
     }, 1000);
+}
+function Gameover(){  
+    document.getElementById('end').innerHTML = hp+'♡';
 }
 
